aa9d9518c39e7467a262efb3a6474b854a3d0ff6
Swastik Bose <swastikbose31@gmail.com>
2025-12-25 20:55:18 +0530

    ui updates

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -31,7 +31,7 @@ function Signup() {
 
     return (
         <>
-            <div className='flex items-center justify-center'>
+            <div className='flex items-center justify-center w-full h-full'>
                 <div className={`mx-auto w-full max-w-lg bg-blue-100 rounded-xl p-10 border border-black/10`}>
                     <div className='mb-2 flex justify-center'>
                         <span className='inline-block w-full max-w-[100px]'>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,10 +2,10 @@ import SignupForm from "../components/auth/Signup"
 
 function Signup() {
     return (
-        <div className='py-8'>
+        <div className='p-8 w-full h-full flex items-center justify-center'>
             <SignupForm />
         </div>
     )
 }
 
-export default Signup
+export default Signup;
